8ef9f54b1d96106cd1c0ff01f00dc58419060af0
Vishal Vashisht <vish@phatmonkey.co.uk>
2026-02-18 08:50:17 +0000

    new providers

--- providers.tf
+++ providers.tf
@@ -15,27 +15,72 @@ terraform {
       version = "~> 6.36.1"
     }
 
-    nutanix = {
-      source = "nutanix/nutanix"
-      version = "2.4.0"
-    }
+    # nutanix = {
+    #   source = "nutanix/nutanix"
+    #   version = "2.4.0"
+    # }
 
-    vcf = {
-      source = "vmware/vcf"
-      version = "0.17.1"
-    }
+    # vcf = {
+    #   source = "vmware/vcf"
+    #   version = "0.17.1"
+    # }
 
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
+    # docker = {
+    #   source  = "kreuzwerker/docker"
+    #   version = "~> 2.13.0"
+    # }
 
-    maas = {
-      source = "canonical/maas"
-      version = "2.7.2"
-    }
+    # maas = {
+    #   source = "canonical/maas"
+    #   version = "2.7.2"
+    # }
   
   }
     
   }
 
+provider "aws" {
+    region = var.aws_region
+    default_tags {
+        tags = {
+            "CreatedBy" = "Vishybear"
+            "Environment" = var.environment
+            "Project" = var.project_name
+        }
+    }
+  
+}
+
+provider "azurerm" {
+  features {}
+  default_tags {
+    tags = {
+      "CreatedBy" = "Vishybear"
+      "Environment" = var.environment
+      "Project" = var.project_name
+    }
+  }
+}   
+
+provider "google" {
+
+    project = var.gcp_project
+    region  = var.gcp_region
+    zone    = var.gcp_zone
+
+
+}
+
+# //provider "nutanix" {
+    
+# //}
+
+# //provider "vcf" {
+
+# //}
+
+# //provider "docker" {
+# //}
+
+# //provider "maas" {
+# //}
